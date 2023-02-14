c171e1ce38a4fb452f820acc6528c7b2a96e5f92
saajankikani <skikani@umich.edu>
2023-02-14 15:39:58 -0500

    Indicator file created

---
+++ FinAnalysis/Indicators.cpp
@@ -0,0 +1,38 @@
+//
+//  Indicators.cpp
+//  FinAnalysis
+//
+//  Created by Saajan Kikani on 2/14/23.
+//
+
+#include "Indicators.hpp"
+
+std::vector<double> dcf(int years, double init_fcf, double rate){
+    std::vector<double> res;
+    
+    return res;
+}
+
+double sharpe(){
+    return 0.0;
+}
+
+double simple_mov_avg(int roll){
+    return 0.0;
+}
+
+double exp_mov_avg(int roll){
+    return 0.0;
+}
+
+double disc_resid_inc(){
+    return 0.0;
+}
+
+double obv(){
+    return 0.0;
+}
+
+double rsi(){
+    return 0.0;
+}
---
+++ FinAnalysis/Indicators.hpp
@@ -0,0 +1,28 @@
+//
+//  Indicators.hpp
+//  FinAnalysis
+//
+//  Created by Saajan Kikani on 2/14/23.
+//
+
+#ifndef Indicators_hpp
+#define Indicators_hpp
+
+#include <stdio.h>
+#include <vector>
+
+std::vector<double> dcf(int years, double init_fcf, double rate);
+
+double sharpe();
+
+double simple_mov_avg(int roll);
+
+double exp_mov_avg(int roll);
+
+double disc_resid_inc();
+
+double obv();
+
+double rsi();
+
+#endif /* Indicators_hpp */
