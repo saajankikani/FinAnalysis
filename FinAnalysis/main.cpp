f29f3af15580780b906a985f1cc0a3703fafab68
saajankikani <skikani@umich.edu>
2023-02-14 14:24:36 -0500

    Initial Commit

---
+++ FinAnalysis/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FinAnalysis
+//
+//  Created by Saajan Kikani on 2/14/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
