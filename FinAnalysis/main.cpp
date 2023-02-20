cad319b5f66b1e2e8472b9334a64624cd7653d4a
saajankikani <skikani@umich.edu>
2023-02-20 15:23:28 -0500

    v1.1.2 implement main suite

--- FinAnalysis/main.cpp
+++ FinAnalysis/main.cpp
@@ -6,9 +6,51 @@
 //
 
 #include <iostream>
+#include "Indicators.hpp"
+#include "DataWrangle.hpp"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout<<"Enter USERID: ";
+    int username;
+    std::cin >> username;
+
+    std::cout<<"Input filename: ";
+    std::string file_in;
+    std::cin>> file_in;
+
+    DataWrangle data(file_in);
+
+    std::cout<<"Run Entire package? [y/n]: ";
+    char answer;
+    std::cin >> answer;
+
+    if(answer == 'y'){
+        std::cout<<"Simple Moving Average: "<< simple_mov_avg(data, 50)<<'\n';
+        std::cout<<"Exponential Moving Average: "<< exp_mov_avg(data, 2, 50)<<'\n';
+        std::cout<<"Sharpe Ratio: "<<sharpe(data)<<'\n';
+    }else{
+        std::cout<<"Which packages would you like to see?"<<'\n';
+        std::cout<<'\t'<<"[1] Simple Moving Average"<<'\n';
+        std::cout<<'\t'<<"[2] Exponential Moving Average"<<'\n';
+        std::cout<<'\t'<<"[3] Sharpe Ratio"<<'\n';
+
+        std::string answer;
+        std::cin>>answer;
+
+        if(answer == "1"){
+        
+            std::cout<<"Simple Moving Average: "<< simple_mov_avg(data, 50)<<'\n';
+
+        }else if(answer == "2"){
+
+            std::cout<<"Exponential Moving Average: "<< exp_mov_avg(data, 2, 50)<<'\n';
+      
+        }else if(answer == "3"){        
+        
+            std::cout<<"Sharpe Ratio: "<<sharpe(data)<<'\n';
+        }
+    }
+
     return 0;
 }
